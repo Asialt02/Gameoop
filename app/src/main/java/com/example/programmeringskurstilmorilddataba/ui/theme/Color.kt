fccb71beb99965e105ddd4efb5dfb0b775f9b4fe
Martin A. Nilsen <martinaleks11@gmail.com>
2025-03-23 14:50:18 +0100

    Test branch commit

--- app/src/main/java/com/example/programmeringskurstilmorilddataba/ui/theme/Color.kt
+++ app/src/main/java/com/example/programmeringskurstilmorilddataba/ui/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//Courses and modules
+
+val InfoCardColor = Color(0xFFefe0fc)
+
+val PrimaryPurple = Color(0xFF6f16bb)
+
+val BeginnerDiffColor = Color(0xFF008000)
+val IntermediateDiffColor = Color(0xFFffa500)
+val AdvancedDiffColor = Color(0xFFff0000)
+val MasterDiffColor = Color(0xFF800000)
+val PlaceholderDiffColor = Color(0xFF000000)
