b26fd80d102d575706455a7a431e99767017bb1c
Martin A. Nilsen <martinaleks11@gmail.com>
2025-03-23 14:52:01 +0100

    Test branch commit

---
+++ app/src/main/java/com/example/programmeringskurstilmorilddataba/CourseData.kt
@@ -0,0 +1,148 @@
+package com.example.programmeringskurstilmorilddataba
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+fun loadCourseDataFromDatabase() {
+    val db = FirebaseFirestore.getInstance()
+
+    var data = "If you see this something fucked up"
+
+    db.collection("courses_v2_(Testing)").document("courseID1Testing").get()
+        .addOnSuccessListener { document ->
+            if (document.exists()) {
+                data = document.get("courseName").toString()
+                println(data)
+            }
+
+        }
+        .addOnFailureListener { e ->
+            println(e.message)
+        }
+
+    println(data)
+}
+
+data class CourseInformation(
+    val id: Int = 0,
+    val name: String = "Placeholder",
+    val description: String = "Placeholder",
+    val noOfModules: Int = 0,
+
+    var completedModules: Int = 0,
+
+    var unlocked: Boolean = false,
+
+    val containedModules: List<ModuleInformation> = listOf()
+)
+
+data class ModuleInformation(
+    val id: Int = 0,
+    val name: String = "Placeholder",
+    val description: String = "Placeholder",
+    val difficulty: String = "Placeholder",
+    val noOfChapters: Int = 0,
+
+    var completedChapters: Int = 0
+)
+
+class DummyCourse {
+    fun loadFirstCourseInformation(): CourseInformation {
+        return CourseInformation(
+            id = 1,
+            name = "Test course",
+            description = "For demonstrating UI",
+            noOfModules = 4,
+            unlocked = true,
+            containedModules = listOf(
+                ModuleInformation(
+                    id = 1,
+                    name = "Variables",
+                    description = "Introduction to use of variables",
+                    difficulty = "Easy",
+                    noOfChapters = 6
+                ),
+                ModuleInformation(
+                    id = 2,
+                    name = "Loops",
+                    description = "Introduction to use of loops",
+                    difficulty = "Easy",
+                    noOfChapters = 8
+                ),
+                ModuleInformation(
+                    id = 3,
+                    name = "The rest",
+                    description = "Every other programming skill",
+                    difficulty = "Very hard",
+                    noOfChapters = 3981
+                )
+            )
+        )
+    }
+
+    fun loadMultiCoursesInformation(): List<CourseInformation> {
+        return listOf(
+            CourseInformation(
+                id = 1,
+                name = "Test course",
+                description = "For demonstrating UI",
+                noOfModules = 4,
+                unlocked = true,
+                containedModules = listOf(
+                    ModuleInformation(
+                        id = 1,
+                        name = "Variables",
+                        description = "Introduction to use of variables",
+                        difficulty = "Beginner",
+                        noOfChapters = 6
+                    ),
+                    ModuleInformation(
+                        id = 2,
+                        name = "Loops",
+                        description = "Introduction to use of loops",
+                        difficulty = "Intermediate",
+                        noOfChapters = 8
+                    ),
+                    ModuleInformation(
+                        id = 3,
+                        name = "The rest",
+                        description = "Every other programming skill",
+                        difficulty = "Advanced",
+                        noOfChapters = 3981
+                    )
+                )
+            ),
+            CourseInformation(
+                id = 2,
+                name = "Second test course",
+                description = "Lorem ipsum",
+                noOfModules = 2,
+                containedModules = listOf(
+                    ModuleInformation(
+                        id = 4,
+                        name = "Me when i test"
+                    ),
+                    ModuleInformation(
+                        id = 5
+                    ),
+                    ModuleInformation(
+                        id = 6
+                    )
+                )
+            ),
+            CourseInformation(
+                containedModules = listOf(
+                    ModuleInformation(
+                        id = 7
+                    ),
+                    ModuleInformation(
+                        id = 8
+                    ),
+                    ModuleInformation(
+                        id = 9
+                    )
+                )
+            )
+        )
+    }
+}
+
